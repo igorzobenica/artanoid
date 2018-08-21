0655133eca5208a55bb3486adca5a12f405898f1
Asus <Asus@DESKTOP-TJEJDMS>
2018-08-21 19:33:09 +0200

    sound test walls

--- Engine/Game.h
+++ Engine/Game.h
@@ -25,6 +25,7 @@
 #include "Graphics.h"
 #include "Ball.h"
 #include "FrameTimer.h"
+#include "Sound.h"
 
 class Game
 {
@@ -47,5 +48,6 @@ private:
 	FrameTimer ft;
 	Ball ball;
 	RectF walls;
+	Sound soundPad;
 	/********************************/
 };
