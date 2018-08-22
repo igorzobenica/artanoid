f8eb14cc1f2705a92da48c04010cf9d0dd242312
Asus <Asus@DESKTOP-TJEJDMS>
2018-08-22 20:43:47 +0200

    brick padding

--- Engine/RectF.h
+++ Engine/RectF.h
@@ -11,6 +11,7 @@ public:
 	RectF(const Vec2& topLeft, float width, float height);
 	bool isOverLappingWith(const RectF& other) const;
 	static RectF FromCenter(const Vec2& center, float halfWidth, float halfHeight);
+	RectF GetExpanded(float offset) const;
 public:
 	float left;
 	float top;
