910f8bd414d3c9c2c2eb8f2a4d618add3500685a
Asus <Asus@DESKTOP-TJEJDMS>
2018-08-24 19:06:02 +0200

    added walls

---
+++ Engine/Walls.cpp
@@ -0,0 +1,21 @@
+#include "Walls.h"
+#include <assert.h>
+
+Walls::Walls(const RectF innerBounds_in, float thickness_in, Color baseColor)
+	:
+	innerBounds(innerBounds_in),
+	thickness(thickness_in),
+	bev(baseColor)
+{
+	assert(innerBounds.GetExpanded(thickness).IsContainedBy(Graphics::GetScreenRect()));
+}
+
+const RectF & Walls::GetInnerBounds() const
+{
+	return innerBounds;
+}
+
+void Walls::Draw(Graphics & gfx) const
+{
+	bev.DrawBevelFrame(innerBounds.GetExpanded(thickness), int(thickness), gfx);
+}
